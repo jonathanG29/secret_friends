743b9ad2d9b283b78a8e65238990471050f091f9
Jonathan Gallego Espinosa <jonathan.gallego7@gmail.com>
2025-02-25 21:13:40 -0500

    ✨ See the list of friends

--- app.js
+++ app.js
@@ -4,23 +4,30 @@ let friends = [];
 
 // Función para agregar un amigo a la lista
 function agregarAmigo() {
-
-    let amigoAñadido = document.querySelector('#amigo').value
+    let amigoAñadido = (document.querySelector('#amigo').value)
 
     // Validar que el campo no esté vacío y que el amigo no haya sido agregado
     if (amigoAñadido === '') {
         alert('Debes ingresar el nombre de tu amigo')
         return
+    }else if (amigoAñadido.length <= 2) {
+        alert('el nombre del amigo debe ser mayor a 2 caracteres')
+        limpiarCaja()
+        return
     }else if (friends.includes(amigoAñadido)) {
         alert('El amigo ya fue agregado')
+        limpiarCaja()
+        return
+    } else if (!isNaN(amigoAñadido)) {
+        alert('El nombre del amigo no puede ser un número')
         limpiarCaja()
         return
     }
 
     friends.push(amigoAñadido)
     console.log(friends)
-    
     limpiarCaja()
+    mostrarAmigos()
 }
 
 // funcion para limpiar la caja de texto
@@ -29,3 +36,15 @@ function limpiarCaja(){
 }
 
 
+// Función para mostrar la lista de amigos en el DOM
+function mostrarAmigos() {
+    let lista = document.querySelector('#listaAmigos');
+    console.log(lista)
+    lista.innerHTML = ''; // Limpiar la lista existente
+
+    for (let amigo of friends) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
